9fd6f1fc2ae25382fd1f8429aa08b90295e7991b
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-05 17:00:35 +0100

    Dodano nazwy, producenta i ceny trzech produktów spożywczych i wyświetlono je w konsoli.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,21 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        String prod1Name = "Masło";
+        String prod1Manufacturer = "Mlekovita";
+        double prod1Price = 7.99;
+
+        String prod2Name = "Corn Flakes";
+        String prod2Manufacturer = "Nestle";
+        double prod2Price = 2.50;
+
+        String prod3Name = "Milka Choco Jelly";
+        String prod3Manufacturer = "Milka";
+        double prod3Price = 12.99;
+
+        System.out.println(prod1Name + " " + prod1Manufacturer + " " + prod1Price + "zł");
+        System.out.println(prod2Name + " " + prod2Manufacturer + " " + prod2Price + "zł");
+        System.out.println(prod3Name + " " + prod3Manufacturer + " " + prod3Price + "zł");
 
     }
 }
